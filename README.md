708ac63c4eb00cb79a118227546920aec2c76ed0
Abhaydeep21 <79368645+Abhaydeep21@users.noreply.github.com>
2021-04-10 15:35:46 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Tranquil_Hackulus
-This is a project for the SIAM-VIT Hackulus hackathon where we have come up with a way to assist people who suffer from mental health ailments without having to actually go and consult a therapist or psychiatrist.
+# mentalhealthtrail2
---
+++ landing.js
@@ -0,0 +1,13 @@
+var content = 'Don’t let life discourage you, everyone who got where he is had to begin where he was. . .';
+
+var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
+
+
+$(ele).hide().appendTo('p').each(function (i) {
+    $(this).delay(100 * i).css({
+        display: 'inline',
+        opacity: 0
+    }).animate({
+        opacity: 1
+    }, 100);
+});
